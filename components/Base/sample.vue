4ac9867305f8c17daf5089657d7d8ce77bf5a721
maruf12 <maruf.harsono@gmail.com>
2023-03-27 10:22:50 +0700

    add new sample base component

---
+++ components/Base/sample.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col bg-green-700 rounded-lg py-5 px-5 mx-3">
+    <span>
+      Welcome, <slot />
+    </span>
+  </div>
+</template>
+
+<script setup>
+defineComponent({
+  name: "BaseSample",
+});
+</script>
