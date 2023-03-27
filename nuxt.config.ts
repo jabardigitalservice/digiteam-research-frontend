68febae358794d11613bd4b41505e6db75693b18
maruf12 <maruf.harsono@gmail.com>
2023-03-27 10:22:34 +0700

    remove module nuxt pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,6 @@ export default defineNuxtConfig({
   ssr: false,
   modules: [
     "@nuxtjs/tailwindcss",
-    "@kevinmarrec/nuxt-pwa",
     ["@pinia/nuxt", { autoImports: ["defineStore"] }],
   ],
   imports: {
@@ -25,18 +24,6 @@ export default defineNuxtConfig({
             "width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0",
         },
       ],
-    },
-  },
-  pwa: {
-    workbox: { enabled: false },
-    meta: {
-      title: "Digiteam 2.0",
-      author: "JDS",
-      mobileAppIOS: true,
-    },
-    manifest: {
-      name: "Digiteam 2.0",
-      short_name: "Digiteam PWA",
     },
   },
 });
