8806dce606bd4ab406e1a2f79cf27c6bf2abf4fd
maruf12 <maruf.harsono@gmail.com>
2023-03-27 14:04:03 +0700

    Add component BaseJdsButton

---
+++ components/Base/Jds/Button/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <button
+    class="mt-3 text-sm font-inter font-medium w-full rounded-lg px-6 py-2 block bg-blue-700 text-white"
+  >
+    Button Layers
+    <slot />
+  </button>
+</template>
