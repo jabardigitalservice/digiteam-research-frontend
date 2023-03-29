b81eedb6286b71ea0e03f182be2eea636558b5b2
maruf12 <maruf.harsono@gmail.com>
2023-03-29 11:16:54 +0700

    testing button

--- components/Base/Jds/Button/index.vue
+++ components/Base/Jds/Button/index.vue
@@ -2,7 +2,7 @@
   <button
     class="mt-3 text-sm font-inter font-medium w-full rounded-lg px-6 py-2 block bg-blue-700 text-white"
   >
-    Button Layers
+    Demo
     <slot />
   </button>
 </template>
